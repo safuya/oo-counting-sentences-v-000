52c1347283e3eb16cd4193e427e4387e6ee82704
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-26 06:54:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/\.$/) ? true : false
   end
 
   def question?
-
+    self.match(/\?$/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/!$/) ? true : false
   end
 
   def count_sentences
-
+    sentences = 0
+    self.split(" ").each do |word|
+      sentences += 1 if word.sentence? || word.question? || word.exclamation?
+    end
+    sentences
   end
 end
